45adc0029203eaa5c8988b359e2adf44e364dbf2
Himanshu Saini <himanshu.singhodiya@gmail.com>
2026-01-30 17:43:51 +0530

    Fix User model import path for Render deployment

--- server/routes/favorites.js
+++ server/routes/favorites.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
-import User from '/models/User.js';
+import User from '../models/user.js';
 const router = express.Router();
 
 /**
